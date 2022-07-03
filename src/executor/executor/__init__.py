06820f4620b7af1eaa77d933a629f2dc9c5f6bc1
Sami Jaghouar <sami.jaghouar@jina.ai>
2022-07-03 16:15:44 +0200

    feat: add comment in python code

--- src/executor/executor/__init__.py
+++ src/executor/executor/__init__.py
@@ -3,11 +3,10 @@ import time
 class MySlowToLoadExec():
 
     def __init__(self):
-        time.sleep(1)
+        time.sleep(1) # It simulates a slow Executor initialization like a deep learning model loading.
+        # I added this line to be sure that the Executor is not re initialized at each called of the http server.
+        # Otherwise each call will take at least one second
 
     def foo(self):
-        print('hello rust world')
-        return 'hello web world'
-
-def executor_foo():
-    print('hello rust world')
+        print('hello rust world') # this will be printed on the server side
+        return 'hello web world' # this will be returned to the client side
